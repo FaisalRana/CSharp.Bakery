caa76149828b758293b939580ce25fdac4f8a592
Faisal Rana <inquisitive@gmail.com>
2021-04-23 16:02:15 -0700

    test 1 good fail

--- Bakery.Tests/ModelTests/BakeryTests.cs
+++ Bakery.Tests/ModelTests/BakeryTests.cs
@@ -7,16 +7,17 @@ using System.Collections.Generic;
 namespace Bakery.Tests
 {
     [TestClass]
-    public class ScrabbleTests
+    public class BakeryTests
     {
         [TestMethod]
         public void BuyBread_CalculatePricefor1LoafofBread_5()
         {
             //Arrange
-          
+            Bread testBread = new Bread();
             //Act
-          
+            int result = testBread.BuyBread(1);
             //Assert
+            Assert.AreEqual(5,result);
         }
     }
 }
