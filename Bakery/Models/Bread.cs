54d070e9ce229587a9bd75eb053ea4933425a152
Faisal Rana <inquisitive@gmail.com>
2021-04-23 17:35:37 -0700

    test 5 good pass

--- Bakery/Models/Bread.cs
+++ Bakery/Models/Bread.cs
@@ -8,11 +8,11 @@ namespace Bakery.Models
         public int CalcBread(int numOfBread) {
           int totalnumBread = numOfBread;
           if(numOfBread % 3 == 0) {
-          //   return (numOfBread * 5) - ((numOfBread/3) * 5);     
-          // } else if(numOfBread % 3 == 1) {
-          //   return (numOfBread * 5) - ((numOfBread-1)/3 * 5);
-          // } else if (numOfBread % 3 == 2) {
-          //   return (numOfBread * 5) - ((numOfBread-2)/3 *5);
+            return (numOfBread * 5) - ((numOfBread/3) * 5);     
+          } else if(numOfBread % 3 == 1) {
+            return (numOfBread * 5) - ((numOfBread-1)/3 * 5);
+          } else if (numOfBread % 3 == 2) {
+            return (numOfBread * 5) - ((numOfBread-2)/3 *5);
           } else return numOfBread * 5;
         }
          
